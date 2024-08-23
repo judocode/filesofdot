90d27919516334c0c80f70e021077be0e7a6fab3
Justus <jrenton87@gmail.com>
2024-08-23 11:58:59 -0500

    allow auto restore sessions

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -237,7 +237,12 @@ require('lazy').setup({
     'rmagatti/auto-session',
     config = function()
       require("auto-session").setup {
-        auto_session_allowed_dirs = { "~/Web/" },
+        auto_session_enable_last_session = true,
+        auto_session_root_dir = vim.fn.stdpath('data').."/sessions/",
+        auto_session_enabled = true,
+        auto_save_enabled = true,
+        auto_restore_enabled = true,
+        auto_session_suppress_dirs = nil
       }
     end,
   },
