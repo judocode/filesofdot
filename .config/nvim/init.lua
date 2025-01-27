2a22e8066c96d5045baf3213800fb795542767e3
Justus <jrenton87@gmail.com>
2025-01-27 14:36:28 -0600

    add conform

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -101,7 +101,50 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
-
+  { -- Autoformat
+    'stevearc/conform.nvim',
+    event = { 'BufWritePre' },
+    cmd = { 'ConformInfo' },
+    keys = {
+      {
+        '<leader>fb',
+        function()
+          require('conform').format { async = true, lsp_format = 'fallback' }
+        end,
+        mode = '',
+        desc = '[F]ormat [B]uffer',
+      },
+    },
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
+        return {
+          timeout_ms = 500,
+          lsp_format = lsp_format_opt,
+        }
+      end,
+      formatters_by_ft = {
+        -- lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use 'stop_after_first' to run the first available formatter from the list
+        javascript = { "prettierd", "prettier", stop_after_first = true, lsp_format = 'first' },
+        typescriptreact = { "prettier", stop_after_first = true, lsp_format = 'first' },
+        typescript = { "prettierd", "prettierd", "prettier", stop_after_first = true, lsp_format = 'first' },
+      },
+    },
+  },
   {
     -- Autocompletion
     'hrsh7th/nvim-cmp',
@@ -623,21 +666,21 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, 'shiftwidth', 2)
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
-
-  if client.server_capabilities.documentFormattingProvider then
-    local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      group = group,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format({ bufnr = bufnr, async = true })
-        -- vim.cmd('write')
-      end,
-    })
-  end
+  -- vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+  --   vim.lsp.buf.format()
+  -- end, { desc = 'Format current buffer with LSP' })
+  --
+  -- if client.server_capabilities.documentFormattingProvider then
+  --   local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
+  --   vim.api.nvim_create_autocmd("BufWritePost", {
+  --     group = group,
+  --     buffer = bufnr,
+  --     callback = function()
+  --       -- vim.lsp.buf.format({ bufnr = bufnr, async = true })
+  --       -- vim.cmd('write')
+  --     end,
+  --   })
+  -- end
 end
 
 -- mason-lspconfig requires that these setup functions are called in this order
