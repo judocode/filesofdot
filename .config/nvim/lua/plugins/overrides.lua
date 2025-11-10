bd19322d0f072fb245d658dda43072b9be01371d
Justus <jrenton87@gmail.com>
2025-11-10 12:55:13 -0600

    add shortcut for notes

--- .config/nvim/lua/plugins/overrides.lua
+++ .config/nvim/lua/plugins/overrides.lua
@@ -1,5 +1,17 @@
 return {
   {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true, -- Show hidden files by default
+          hide_dotfiles = false, -- Do not hide files starting with '.'
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
+  {
     "hrsh7th/nvim-cmp",
     enabled = false,
   },
