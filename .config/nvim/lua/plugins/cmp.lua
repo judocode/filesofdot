8df7428c3fbb083721c5236eaf96541f39f7686b
Justus <jrenton87@gmail.com>
2025-12-14 01:54:48 -0600

    customize lazyvim

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -5,40 +5,76 @@ return {
   },
   {
     "saghen/blink.cmp",
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.config
     opts = {
-      term = {
-        sources = {
-          default = {
-            "lsp",
-            -- "path",
-            -- "snippets"
-            "buffer",
-          },
-          transform_items = function(_, items)
-            return vim.tbl_filter(function(item)
-              return item.kind ~= require("blink.cmp.types").CompletionItemKind.Snippet
-            end, items)
-          end,
-          providers = {
-            snippets = {
-              opts = {
-                enabled = false,
-                friendly_snippets = false,
-              },
-            },
-          },
-        },
+      appearance = {
+        use_nvim_cmp_as_default = false,
+        nerd_font_variant = "normal",
+      },
+      cmdline = {
+        keymap = { preset = "inherit" },
+        completion = { menu = { auto_show = true } },
       },
       completion = {
-        trigger = {
-          show_on_blocked_trigger_characters = { "." },
-        },
-        menu = {
+        accept = { auto_brackets = { enabled = true } },
+        documentation = {
           auto_show = true,
-          auto_show_delay_ms = 250,
+          auto_show_delay_ms = 300,
+          treesitter_highlighting = true,
+          update_delay_ms = 50,
+          window = { border = "rounded" },
         },
         list = {
-          max_items = 4,
+          selection = {
+            preselect = true,
+            auto_insert = false,
+          },
+          max_items = 2,
+        },
+        menu = {
+          border = "rounded",
+          draw = {
+            columns = {
+              { "label", "label_description", gap = 1 },
+              { "kind_icon", "kind" },
+            },
+            treesitter = { "lsp" },
+          },
+          auto_show = true,
+          auto_show_delay_ms = 300,
+        },
+        trigger = { show_in_snippet = false },
+      },
+
+      signature = {
+        enabled = true,
+        window = { border = "rounded", show_documentation = false },
+      },
+
+      sources = {
+        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+        providers = {
+          lazydev = {
+            name = "lazydev",
+            module = "lazydev.integrations.blink",
+            -- make lazydev completions top priority (see `:h blink.cmp`)
+            score_offset = 1000,
+          },
+          lsp = {
+            min_keyword_length = 2,
+            score_offset = 0,
+          },
+          path = {
+            min_keyword_length = 1,
+          },
+          snippets = {
+            min_keyword_length = 2,
+          },
+          buffer = {
+            min_keyword_length = 4,
+            max_items = 5,
+          },
         },
       },
     },
---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = function(_, opts)
+      opts.event_handlers = {
+        {
+          event = "neo_tree_buffer_enter",
+          handler = function(arg)
+            vim.cmd([[
+            setlocal relativenumber
+          ]])
+          end,
+        },
+      }
+    end,
+  },
+}
